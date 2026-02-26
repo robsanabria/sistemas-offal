6657ef3d91db56fe3a1e2b40f813d9cea553b801
Rob! <sanabria.er@gmail.com>
2026-02-26 00:28:48 -0300

    Fix: KV conexion a db de vercel

--- src/lib/kv.ts
+++ src/lib/kv.ts
@@ -1,14 +1,11 @@
-import { Redis } from '@upstash/redis'
+import { kv } from '@vercel/kv'
 
-export const kv = new Redis({
-  url: process.env.KV_REST_API_URL!,
-  token: process.env.KV_REST_API_TOKEN!,
-})
+export { kv }
 
 // Keys for our office data
 export const KEYS = {
-  ANDREA_ABSENCE: 'office:andrea:absence',
-  SPEED_COUNTER: 'office:speeds:total',
-  COFFEE_MAKER: 'office:coffee:maker',
-  OFFICE_MATES: 'office:team:members',
+    ANDREA_ABSENCE: 'office:andrea:absence',
+    SPEED_COUNTER: 'office:speeds:total',
+    COFFEE_MAKER: 'office:coffee:maker',
+    OFFICE_MATES: 'office:team:members',
 }
